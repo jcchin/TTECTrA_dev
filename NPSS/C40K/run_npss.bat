0d5cd5a79a4cfb77842e8b0dfcd848c08a881ecf
jcchin <jchin89@gmail.com>
2014-07-15 22:13:02 -0400

    fresh run_npss batch files (path free). from now on: add paths to npss_paths (git ignored)

---
+++ NPSS/C40K/run_npss.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM Change the npss directory to your local copy's address - Remember to put "\" at the end of your address
+set NPSS_TOP=C:\path2npss\
+
+IF EXIST ..\npss_paths.bat (
+    REM Check for saved private paths (this will overwrite the one set above)
+    call ..\npss_paths.bat
+)
+
+set NPSS_CONFIG=nt
+set NPSS_DEV_TOP=%NPSS_TOP%\DLMdevkit
+
+REM This path for Matlab level
+REM "%NPSS_TOP%bin\npss.nt.exe" -I "%NPSS_TOP%InterpIncludes" -I "%NPSS_TOP%MetaData" -I "%NPSS_TOP%DLMComponents\nt" -I "%NPSS_TOP%InterpComponents" -I "150PAX_Sfunction\src" -I "150PAX_Sfunction\view" -I "150PAX_Sfunction\run" %* 
+REM This path for NPSS level
+"%NPSS_TOP%bin\npss.nt.exe" -I "%NPSS_TOP%InterpIncludes" -I "%NPSS_TOP%MetaData" -I "%NPSS_TOP%DLMComponents\nt" -I "%NPSS_TOP%InterpComponents" -I "src" -I "maps" -I "view" -I "run" %* 
