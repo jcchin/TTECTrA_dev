772d8819edf617e1b66cc453b474e1b7bf6c5006
mfozcan <metin.ozcan@gatech.edu>
2014-09-23 11:10:17 -0400

    Minor Matlab input changes

--- Matlab/set_paths.m
+++ Matlab/set_paths.m
@@ -1,6 +1,10 @@
 %computer path dependent setup:
 %add your path here, or in a separate file called paths.m (if using version control):
+<<<<<<< a78697e27cc44f5eb22b96fdbf23096da563add7
 npss_location = ' '; %path location of the S-function capable NPSS executable
+=======
+npss_location = 'C:\Users\mozcan3\Desktop\NPSS.nt.sfunction2'; %path location of the S-function capable NPSS executable
+>>>>>>> b6ca9495123e29996a3c5498f04d8646484bd2e9
 
 %----No need to edit below here ---%
 if (exist('paths.m', 'file') == 2) %load paths if supplied elsewhere
