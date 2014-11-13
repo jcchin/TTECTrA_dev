40d212d50b37863c55bbc12581ca7770e86cf3b2
jcchin <jchin89@gmail.com>
2014-11-12 20:23:18 -0500

    trimmed maps

--- Matlab/TTECTrA_Auto.m
+++ Matlab/TTECTrA_Auto.m
@@ -27,7 +27,7 @@
 %       bkpt
 % *************************************************************************
 close all; clear; clc;
-
+tic
 % add paths for support and gui functions
 addpath('TTECTrA_Auto')
 
@@ -111,7 +111,7 @@ TTECTrA_IWP_s
 % if isempty(ttectra_in.controller.IWP_gain)
 %     ttectra_in.controller.IWP_gain=1650;
 % end
-
+toc
 %------------------------------
 % Test Controller Design
 %------------------------------
