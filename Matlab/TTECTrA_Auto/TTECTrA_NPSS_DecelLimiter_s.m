85bb6d9f8c23c352392275e9ed2d901aac371ca4
jcsank <jeffrey.t.csank@nasa.gov>
2014-11-14 09:43:17 -0500

    Bug Fix -Modified code to trim off the first second of data when determining minimum FAR, LPC surge margin, and Wf/Ps3 ratios. -Fixed bug where initial limit was the max of the guessed Wf/Ps3 based on FAR and then the actual FAR.  The latter is the issue, it should be the max of guessed Wf/Ps3 and min Wf/Ps3 from derived steady-state relationships.

--- Matlab/TTECTrA_Auto/TTECTrA_NPSS_DecelLimiter_s.m
+++ Matlab/TTECTrA_Auto/TTECTrA_NPSS_DecelLimiter_s.m
@@ -5,11 +5,13 @@
 temp_in=ttectra_in;
 
 dtemp_watchdog_limit=20;
+dtemp_trimi=round(1/temp_in.in.Ts);
+
 %-------------------------------------------
 %Determine what the Wf/Ps3 limit is based on steady-state data
 %-------------------------------------------
 dtemp_WfPs3lim=interp1(temp_in.SP.FAR_SP,temp_in.SP.WfPs3_SP,temp_in.SMLimit.FARmin,'linear','extrap');
-dtemp_WfPs3lim=max(min(temp_in.SMLimit.FARmin),dtemp_WfPs3lim);
+dtemp_WfPs3lim=max(min(temp_in.SP.WfPs3_SP),dtemp_WfPs3lim);
 
 %-------------------------------------------
 % Now add in the decel limiter and determine if WfPs3limit will preseve
@@ -28,7 +30,6 @@ dtemp_watchdog=1;
 dtemp_out=[];
 
 while isempty(dtemp_out) && dtemp_watchdog<10
-    
     if dtemp_watchdog>1
         temp_in.Limiter.WfPs3lim=1.1*temp_in.Limiter.WfPs3lim;
     end
@@ -45,8 +46,8 @@ end
 % Now that we have a valid starting point, fine tune the decel limiter
 %-------------------------------------------
 dtemp_watchdog=1;
-dtemp_WfPs3lim=temp_in.Limiter.WfPs3lim
-dtemp_error=(temp_in.SMLimit.FARmin-min(dtemp_out.FAR))/temp_in.SMLimit.FARmin;
+dtemp_WfPs3lim=temp_in.Limiter.WfPs3lim;
+dtemp_error=(temp_in.SMLimit.FARmin-min(dtemp_out.FAR(dtemp_trimi:end)))/temp_in.SMLimit.FARmin;
 while abs(dtemp_error)>0.10 && dtemp_watchdog<dtemp_watchdog_limit   
     WfPs3lim_prev=dtemp_WfPs3lim;
     
@@ -70,8 +71,8 @@ while abs(dtemp_error)>0.10 && dtemp_watchdog<dtemp_watchdog_limit
     
     try
         dtemp_save_error(dtemp_watchdog)=dtemp_error;
-        dtemp_error=(temp_in.SMLimit.FARmin-min(dtemp_out.FAR))/temp_in.SMLimit.FARmin;
-        dtemp_watchdog=dtemp_watchdog+1;
+        dtemp_error=(temp_in.SMLimit.FARmin-min(dtemp_out.FAR(dtemp_trimi:end)))/temp_in.SMLimit.FARmin;
+        dtemp_watchdog=dtemp_watchdog+1;        
     catch
         dtemp_WfPs3lim=WfPs3lim_prev;
         dtemp_watchdog=dtemp_watchdog_limit;
