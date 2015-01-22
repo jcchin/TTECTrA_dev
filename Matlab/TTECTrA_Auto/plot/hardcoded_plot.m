9f32a622fda3eb19a28161d8d2c74dc25323a41e
jcchin <jchin89@gmail.com>
2015-01-21 19:03:24 -0500

    refactor

--- Matlab/TTECTrA_Auto/plot/hardcoded_plot.m
+++ Matlab/TTECTrA_Auto/plot/hardcoded_plot.m
@@ -28,9 +28,11 @@ Wc = monotonic(Wc); %make monotonically increasing
 Wc = mass_conversion(Wc,'lbm2kg'); %convert units to SI
 
 %save to object
-Wc_map = Wc; %custom scalar
-eta_map = eta;
-pr_map = pr; %custom scalar
+P = [2 1 3]; %reshape in this order
+
+Wc_map = permute(Wc, P);
+eta_map = permute(eta, P);
+pr_map = permute(pr, P);
 Nc_index = Nc;
 Rline_index = Rline;
 Alpha_index = Alpha;
@@ -41,7 +43,6 @@ hold on
 alpha =1;
 op_bool = 0;
 
-colors = ['y' 'm' 'c' 'b' 'r'];
 %% interp speedlines
 % %make 11x13x3 indices
 Rlength = length(Rline_index);
@@ -49,24 +50,14 @@ Nlength = length(Nc_index);
 Alength = length(Alpha_index);
 testx = Wc_map;
 
-gridx = repmat(Rline_index',[1,Nlength,Alength]);
-gridy = repmat(Nc_index,[Rlength,1,Alength]);
-gridz = repmat(shiftdim(Alpha_index,-1), [Rlength,Nlength]);
+gridx = permute(repmat(Rline_index',[1,Nlength,Alength]), P);
+gridy = permute(repmat(Nc_index,[Rlength,1,Alength]), P);
+gridz = permute(repmat(shiftdim(Alpha_index,-1), [Rlength,Nlength]), P);
 
-%aa = ones(Rlength,Nlength)*0;
 aaa = ones(Nlength,Rlength,Alength)*1;
-    
-P = [2 1 3];
-gridx = permute(gridx, P);
-gridy = permute(gridy, P);
-gridz = permute(gridz, P);
-Wc = permute(Wc_map, P);
-pr = permute(pr_map, P);
-eta = permute(eta_map, P);
-
-interp3Wc = interp3(gridx,gridy,gridz,Wc, gridx, gridy, aaa);
-interp3pr = interp3(gridx,gridy,gridz,pr, gridx, gridy, aaa);
-interp3eta = interp3(gridx,gridy,gridz,eta, gridx, gridy, aaa);
+interp3Wc = interp3(gridx,gridy,gridz,Wc_map, gridx, gridy, aaa);
+interp3pr = interp3(gridx,gridy,gridz,pr_map, gridx, gridy, aaa);
+interp3eta = interp3(gridx,gridy,gridz,eta_map, gridx, gridy, aaa);
 
 Wc3 = permute(interp3Wc, P);
 pr3 = permute(interp3pr, P);
@@ -77,13 +68,13 @@ surf(Wc3(:,:,1),pr3(:,:,1),ones(size(Wc3(:,:,1)))*0,eta3(:,:,1)), shading interp
 for i = 1:length(Nc_index)
     for j = 1:Alength
     %pcolor(Wc_map(:,:,alpha),pr_map(:,:,alpha),eta_map(:,:,alpha))
-    plot3(Wc_map(:,:,j),pr_map(:,:,j),ones(Rlength,Nlength)*(Alpha_index(j)), colors(j))
+    plot3(Wc(:,:,j),pr(:,:,j),ones(Rlength,Nlength)*(Alpha_index(j)), 'b')
     end
     hold on
     % Label Speed lines
     if rem(i,2) == 0  %every other line
         label(i) = cellstr(['speed ' num2str(Nc_index(i)) ]);
-        text(Wc_map(1,i), pr_map(1,i), label(i),'VerticalAlignment','bottom','HorizontalAlignment','right');
+        text(Wc(1,i), pr(1,i), label(i),'VerticalAlignment','bottom','HorizontalAlignment','right');
     end
 end
 % Operating Points
@@ -100,7 +91,7 @@ grid on
 %hold on
 for i = 1:length(Rline_index)
     for id = 1:length(Rline_index)
-        plot(Wc_map(i,1:length(Nc_index)),pr_map(i,1:length(Nc_index)),'--g')
+        plot(Wc(i,1:length(Nc_index)),pr(i,1:length(Nc_index)),'--g')
         hold on
     end
 end
