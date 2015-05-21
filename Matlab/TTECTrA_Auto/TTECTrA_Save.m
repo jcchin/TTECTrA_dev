200ae7cee4e7a92116c118673e86f3e0a9780b00
jcsank <jeffrey.t.csank@nasa.gov>
2015-05-21 08:57:34 -0400

    Modified to save only controller if there is no closed loop data available.  This is beneficial if debugging/designing manually from command line.

--- Matlab/TTECTrA_Auto/TTECTrA_Save.m
+++ Matlab/TTECTrA_Auto/TTECTrA_Save.m
@@ -1,3 +1,9 @@
-save([ttectra_in.in.HomeDirectory '\Matlab\NPSSdata\' ttectra_in.in.ttectra_engine_name '\Closed_Loop_Data.mat'],'ttectra_in','out');
+
+if exist('out')
+    save([ttectra_in.in.HomeDirectory '\Matlab\NPSSdata\' ttectra_in.in.ttectra_engine_name '\Closed_Loop_Data.mat'],'ttectra_in','out');
+    disp('Successfully Saved TTECTrA Controller and Closed Loop Data');
+else
+    save([ttectra_in.in.HomeDirectory '\Matlab\NPSSdata\' ttectra_in.in.ttectra_engine_name '\_K_Only.mat'],'ttectra_in');
+    disp('Successfully Saved TTECTrA Controller');
+end
     
-disp('Successfully Saved TTECTrA Controller');
